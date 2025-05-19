224e8f94837acffab571d2df66812b4b67f0df45
Nirav Patel <niravp0703@gmail.com>
2025-05-18 20:01:59 -0400

    feat: add package-info.java files for commands, config, events, listeners, playtime, and util packages

---
+++ src/main/java/com/storytimeproductions/stweaks/config/package-info.java
@@ -0,0 +1,2 @@
+/** This package contains the configuration classes for the STweaks application. */
+package com.storytimeproductions.stweaks.config;
---
+++ src/main/java/com/storytimeproductions/stweaks/playtime/package-info.java
@@ -0,0 +1,2 @@
+/** This package contains the Playtime feature used for the Stracker module for the plugin. */
+package com.storytimeproductions.stweaks.playtime;
