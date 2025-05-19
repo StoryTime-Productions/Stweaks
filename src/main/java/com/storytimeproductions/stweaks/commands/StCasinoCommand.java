176e141f594b689df69e5a978cf2208596bf8482
Nirav Patel <niravp0703@gmail.com>
2025-05-18 21:56:15 -0400

    refactor: reorganize documentation and method structure in StCasinoCommand

--- src/main/java/com/storytimeproductions/stweaks/commands/StCasinoCommand.java
+++ src/main/java/com/storytimeproductions/stweaks/commands/StCasinoCommand.java
@@ -6,8 +6,23 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+/**
+ * Handles the /casino command, teleporting the player to the "casino" world using Multiverse.
+ *
+ * <p>This command can only be executed by players. When run, it dispatches the Multiverse teleport
+ * command as the console to move the player to the "casino" world.
+ */
 public class StCasinoCommand implements CommandExecutor {
 
+  /**
+   * Executes the /casino command.
+   *
+   * @param sender The source of the command.
+   * @param command The command that was executed.
+   * @param label The alias of the command used.
+   * @param args The command arguments.
+   * @return true if the command was handled, false otherwise.
+   */
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (!(sender instanceof Player player)) {
