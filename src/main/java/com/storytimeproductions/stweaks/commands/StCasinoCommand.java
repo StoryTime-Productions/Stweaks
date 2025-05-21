a7a39dec1dcf2720be2868ce255ca1261b8bec64
Nirav Patel <niravp0703@gmail.com>
2025-05-20 23:21:14 -0400

    refactor: improve code formatting and readability in StCasinoCommand and StLobbyCommand

--- src/main/java/com/storytimeproductions/stweaks/commands/StCasinoCommand.java
+++ src/main/java/com/storytimeproductions/stweaks/commands/StCasinoCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 /**
@@ -13,6 +14,8 @@ import org.bukkit.entity.Player;
  * command as the console to move the player to the "casino" world.
  */
 public class StCasinoCommand implements CommandExecutor {
+
+  private FileConfiguration config;
 
   /**
    * Executes the /casino command.
@@ -31,6 +34,26 @@ public class StCasinoCommand implements CommandExecutor {
     }
     // Teleport the player to the casino world using Multiverse
     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mv tp " + player.getName() + " casino");
+
+    // Get location from config (example path: casino-spawn.x, y, z, yaw, pitch)
+    double x = config.getDouble("casino-spawn.x", 0.5);
+    double y = config.getDouble("casino-spawn.y", 64.0);
+    double z = config.getDouble("casino-spawn.z", 0.5);
+    float yaw = (float) config.getDouble("casino-spawn.yaw", 0.0);
+    float pitch = (float) config.getDouble("casino-spawn.pitch", 0.0);
+
+    // Delay the teleport to ensure Multiverse has moved the player to the correct
+    // world
+    Bukkit.getScheduler()
+        .runTaskLater(
+            Bukkit.getPluginManager().getPlugin("stweaks"),
+            () -> {
+              if (player.isOnline() && player.getWorld().getName().equalsIgnoreCase("casino")) {
+                player.teleport(new org.bukkit.Location(player.getWorld(), x, y, z, yaw, pitch));
+              }
+            },
+            10L); // 10 ticks = 0.5 seconds
+
     return true;
   }
 }
